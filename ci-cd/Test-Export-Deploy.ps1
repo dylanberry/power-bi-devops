4a0365301e19268718b416c138daae2d77946b17
dylanberry <dylan@dylanberry.com>
2021-11-16 17:08:56 -0500

    it works!

--- ci-cd/Test-Export-Deploy.ps1
+++ ci-cd/Test-Export-Deploy.ps1
@@ -10,25 +10,25 @@ $bimFolderPath = './bim'
 
 pushd $PSScriptRoot
 
-# echo "Export reports from $sourceWorkspaceName"
-# $params = @{
-#     ClientId = $clientId
-#     ClientSecret = $clientSecret
-#     TenantId = $tenantId
-#     WorkspaceName = $sourceWorkspaceName
-#     PbixFolderPath = $pbixFolderPath
-# }
-# ./export/Export-PowerBIReports.ps1 @params
+echo "Export reports from $sourceWorkspaceName"
+$params = @{
+    ClientId = $clientId
+    ClientSecret = $clientSecret
+    TenantId = $tenantId
+    WorkspaceName = $sourceWorkspaceName
+    PbixFolderPath = $pbixFolderPath
+}
+./export/Export-PowerBIReports.ps1 @params
 
-# echo "Export datasets from $sourceWorkspaceName"
-# $params = @{
-#     ClientId = $clientId
-#     ClientSecret = $clientSecret
-#     TenantId = $tenantId
-#     WorkspaceName = $sourceWorkspaceName
-#     BimFolderPath = $bimFolderPath
-# }
-# ./export/Export-PowerBIDataset.ps1 @params
+echo "Export datasets from $sourceWorkspaceName"
+$params = @{
+    ClientId = $clientId
+    ClientSecret = $clientSecret
+    TenantId = $tenantId
+    WorkspaceName = $sourceWorkspaceName
+    BimFolderPath = $bimFolderPath
+}
+./export/Export-PowerBIDataset.ps1 @params
 
 echo "Ensure $targetWorkspaceName workspace created"
 $params = @{
@@ -39,31 +39,31 @@ $params = @{
 }
 ./deploy/Ensure-PowerBIWorkspace.ps1 @params
 
-# echo "Import datasets to $targetWorkspaceName"
-# $params = @{
-#     ClientId = $clientId
-#     ClientSecret = $clientSecret
-#     TenantId = $tenantId
-#     WorkspaceName = $targetWorkspaceName
-#     BimFolderPath = $bimFolderPath
-# }
-# powershell -version 5.1 ./deploy/Import-PowerBIDatasets.ps1 @params
+echo "Import datasets to $targetWorkspaceName"
+$params = @{
+    ClientId = $clientId
+    ClientSecret = $clientSecret
+    TenantId = $tenantId
+    WorkspaceName = $targetWorkspaceName
+    BimFolderPath = $bimFolderPath
+}
+powershell -version 5.1 ./deploy/Import-PowerBIDatasets.ps1 @params
 
-# echo "Import reports to $targetWorkspaceName"
-# $params = @{
-#     ClientId = $clientId
-#     ClientSecret = $clientSecret
-#     TenantId = $tenantId
-#     WorkspaceName = $targetWorkspaceName
-#     PbixFolderPath = $pbixFolderPath
-# }
-# ./deploy/Import-PowerBIReports.ps1 @params
+echo "Import reports to $targetWorkspaceName"
+$params = @{
+    ClientId = $clientId
+    ClientSecret = $clientSecret
+    TenantId = $tenantId
+    WorkspaceName = $targetWorkspaceName
+    PbixFolderPath = $pbixFolderPath
+}
+./deploy/Import-PowerBIReports.ps1 @params
 
-# echo "Rebind report datasets and update credentials in $targetWorkspaceName"
-# $params = @{
-#     ClientId = $clientId
-#     ClientSecret = $clientSecret
-#     TenantId = $tenantId
-#     WorkspaceName = $targetWorkspaceName
-# }
-# ./deploy/Update-ReportDatasets.ps1 @params
+echo "Rebind report datasets and update credentials in $targetWorkspaceName"
+$params = @{
+    ClientId = $clientId
+    ClientSecret = $clientSecret
+    TenantId = $tenantId
+    WorkspaceName = $targetWorkspaceName
+}
+./deploy/Update-ReportDatasets.ps1 @params
