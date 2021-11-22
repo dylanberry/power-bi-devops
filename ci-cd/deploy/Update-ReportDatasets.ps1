202755ddea15f6e3a348e5ba9094cbbec420a704
dylanberry <dylan@dylanberry.com>
2021-11-22 10:20:27 -0500

    data source update improvements

--- ci-cd/deploy/Update-ReportDatasets.ps1
+++ ci-cd/deploy/Update-ReportDatasets.ps1
@@ -31,44 +31,68 @@ $groupFilter = "name eq '$WorkspaceName'"
 echo "Get workspace $WorkspaceName"
 $groups = Invoke-RestMethod -Method GET -Uri "$baseUri/groups?`$filter=$groupFilter" `
     -Headers $headers
+$groupId = $groups.value[0].id
 
 echo "Get datasets"
-$dataSets = Invoke-RestMethod -Method GET -Uri "$baseUri/groups/$($groups.value[0].id)/datasets" `
+$datasets = Invoke-RestMethod -Method GET -Uri "$baseUri/groups/$groupId/datasets" `
     -Headers $headers
 
 echo "Get reports"
-$reports = Invoke-RestMethod -Method GET -Uri "$baseUri/groups/$($groups.value[0].id)/reports" `
+$reports = Invoke-RestMethod -Method GET -Uri "$baseUri/groups/$groupId/reports" `
     -Headers $headers
 
-echo "Rebind reports to datasets"
-$datasetJson = @{ "datasetId"="$($dataSets.value[0].id)" } | ConvertTo-Json -Compress
+echo "Rebind datasets to reports and update dataset datasource credentials"
+foreach ($dataset in $datasets.value) {
+  echo "Found dataset $($dataset.name)"
+  $reportFilter = $dataset.name.ToLower().Replace(' dataset', '')
+  $reportFilter
+  $report = $reports.value | ? name -like "*$reportFilter"
+  echo "Found report $($report.name)"
+  $datasetJson = @{ "datasetId"="$($dataset.id)" } | ConvertTo-Json -Compress
 
-Invoke-RestMethod -Method POST -Uri "$baseUri/groups/$($groups.value[0].id)/reports/$($reports.value[0].id)/Rebind" `
-    -Headers $headers `
-    -Body $datasetJson -ContentType 'application/json' -Verbose
+  Invoke-RestMethod -Method POST -Uri "$baseUri/groups/$groupId/reports/$($report.id)/Rebind" `
+      -Headers $headers `
+      -Body $datasetJson -ContentType 'application/json' -Verbose
 
-echo "Rebind reports to datasets"
-$datasources = Invoke-RestMethod -Method GET -Uri "$baseUri/groups/$($groups.value[0].id)/datasets/$($dataSets.value[0].id)/datasources" `
-    -Headers $headers
+  echo "Get datasources"
+  $datasources = Invoke-RestMethod -Method GET -Uri "$baseUri/groups/$groupId/datasets/$($dataset.id)/datasources" `
+      -Headers $headers
 
-foreach ($datasource in $datasources.value) {
-  Invoke-RestMethod -Method GET -Uri "$baseUri/gateways/$($datasource.gatewayId)/datasources/$($datasource.datasourceId)" `
-    -Headers $headers -Verbose
+  foreach ($datasource in $datasources.value) {
+    try {      
+      echo "Datasource type: $($datasource.datasourceType)"
+      $datasource
+      $datasourceDetails = Invoke-RestMethod -Method GET -Uri "$baseUri/gateways/$($datasource.gatewayId)/datasources/$($datasource.datasourceId)" `
+            -Headers $headers -Verbose
 
-  echo 'Update datasource credentials'
-  $credentialJson = @{
-    "credentialDetails"=@{
-      "credentialType"= "Anonymous"
-      "credentials"='{"credentialData":""}'
-      "encryptedConnection"="Encrypted"
-      "encryptionAlgorithm"="None"
-      "privacyLevel"="Public"
-      "useEndUserOAuth2Credentials"=$False
+      switch ($datasource.datasourceType) {
+        'Sql' {  }
+        'File' {  }
+        Default { #'AzureBlobs' 'Web'
+          $datasourceDetails = Invoke-RestMethod -Method GET -Uri "$baseUri/gateways/$($datasource.gatewayId)/datasources/$($datasource.datasourceId)" `
+            -Headers $headers -Verbose
+          $datasourceDetails
+    
+          echo 'Update datasource credentials'
+          $credentialJson = @{
+            "credentialDetails"=@{
+              "credentialType"= "Anonymous"
+              "credentials"='{"credentialData":""}'
+              "encryptedConnection"="Encrypted"
+              "encryptionAlgorithm"="None"
+              "privacyLevel"="Public"
+              "useEndUserOAuth2Credentials"=$False
+            }
+          } | ConvertTo-Json -Compress
+          
+          Invoke-RestMethod -Method PATCH -Uri "$baseUri/gateways/$($datasource.gatewayId)/datasources/$($datasource.datasourceId)" `
+            -Headers $headers `
+            -Body $credentialJson -ContentType 'application/json' -Verbose
+        }
+      }
     }
-  } | ConvertTo-Json -Compress
-
-  echo $credentialJson
-  Invoke-RestMethod -Method PATCH -Uri "$baseUri/gateways/$($datasource.gatewayId)/datasources/$($datasource.datasourceId)" `
-    -Headers $headers `
-    -Body $credentialJson -ContentType 'application/json' -Verbose
+    catch {
+      echo $_.Exception
+    }
+  }
 }
